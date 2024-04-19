c60f8197f80fba7d237719eb5e3b1cb15284a335
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-04-20 02:04:43 +0330

    Update UI
    create a Basic User Interface

--- data/main.js
+++ data/main.js
@@ -3,7 +3,11 @@
 function find(ID) {
     // ID = 505801;
     for (var i = 0; i < banks.length; i++) {
-        if (banks[i].ID == ID) {
+        // console.log(String(banks[i].ID));
+        // console.log(ID)
+
+        
+        if (String(banks[i].ID).includes(ID)) {
             return (banks[i].name)
         }
     }
@@ -12,8 +16,19 @@ function find(ID) {
 var inp = document.querySelector("#intbnkname");
 inp.addEventListener("input", () => {
     let val = inp.value;
+    var fni = find(val);
+    console.log(val.length > 8)
+   
+    if (String(fni) != "undefined") {
+        document.querySelector(".out").innerHTML = fni
+    } 
+    else if(val.length > 8){
+        document.querySelector(".out").innerHTML = "مقدار بیش از حد مجاز ( 8 )"
+    }
+    else {
+        document.querySelector(".out").innerHTML = "تعریف نشده !"
+    }
 
-
-    document.querySelector(".out").innerHTML = find(val)
+    
 
 })
